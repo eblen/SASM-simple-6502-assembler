9ae5e2e724e7a5c5bc56be8153ddb25fef0e8178
John Eblen <jeblen@acm.org>
2013-03-30 22:31:15 -0400

    Allow multiple "org" statements (thus multiple code blocks) Change rule for using "org". It is now mandatory.

--- src/sasm.d
+++ src/sasm.d
@@ -18,6 +18,7 @@
  along with SASM.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
+import std.algorithm;
 import std.c.stdlib;
 import std.conv;
 import std.stdio;
@@ -52,6 +53,15 @@ string byteToHex(ubyte b)
   hex[0] = val2hex[b >> 4];
   hex[1] = val2hex[b & 0x0F];
   return to!string(hex);
+}
+
+struct code_seg
+{
+  ushort org;
+  ubyte[] code;
+  string[ushort] absAddrRef;
+  string[ushort] relAddrRef;
+  string[ushort] zpAddrRef;
 }
 
 void main()
@@ -96,22 +106,19 @@ void main()
     15:'f',
   ];
 
-  int line_num = 1;
-  ushort org = 0;
-  ubyte[] machine_code;
+  code_seg[] code_blocks;
   ushort[string] labelToAddr;
-  string[ushort] absAddrRef;
-  string[ushort] relAddrRef;
-  string[ushort] zpAddrRef;
   auto zpm = new SimpleAppleIIZeroPageManager();
 
   init6502();
+  code_seg* cs = null;
+  int line_num = 1;
   foreach(char[] line; stdin.byLine())
   {
     scope(exit) line_num++;
 
     // Remove comments
-    auto cbegin = indexOf(line, ';');
+    auto cbegin = std.string.indexOf(line, ';');
     if (cbegin > -1) line = line[0..cbegin];
 
     // Create tokens: mnemonic op1 op2
@@ -134,21 +141,11 @@ void main()
         exit(1);
       }
       while (op1.length < 4) op1 = "0" ~ op1;
-      org = 256*hexToByte(op1[0..2]) + hexToByte(op1[2..$]);
-    }
 
-    // Raw data
-    else if (mnemonic == "data")
-    {
-      if (op1.length % 2 == 1)
-      {
-        writefln("Invalid data at line %s", line_num);
-        exit(1);
-      }
-      for (int i=0; i<op1.length; i += 2)
-      {
-        machine_code ~= hexToByte(op1[i..i+2]);
-      }
+      // Create new code segment
+      ++code_blocks.length;
+      cs = &code_blocks[$-1];
+      cs.org = 256*hexToByte(op1[0..2]) + hexToByte(op1[2..$]);
     }
 
     // Request for zbyte
@@ -164,13 +161,34 @@ void main()
       labelToAddr[op1] = zpm.alloc(alloc_size);
     }
 
+    // Enforce that an "org" occurs before all other commands except "zbyte".
+    else if (code_blocks.length == 0)
+    {
+      writeln("Error - org required before all other commands except zbyte");
+      exit(1);
+    }
+
+    // Raw data
+    else if (mnemonic == "data")
+    {
+      if (op1.length % 2 == 1)
+      {
+        writefln("Invalid data at line %s", line_num);
+        exit(1);
+      }
+      for (int i=0; i<op1.length; i += 2)
+      {
+        cs.code ~= hexToByte(op1[i..i+2]);
+      }
+    }
+
     // Label
-    else if (mnemonic[0] == '.') labelToAddr[mnemonic[1..$]] = cast(ushort)(org + machine_code.length);
+    else if (mnemonic[0] == '.') labelToAddr[mnemonic[1..$]] = cast(ushort)(cs.org + cs.code.length);
 
     // Instruction
     else
     {
-      machine_code ~= proc6502.bytecode(mnemonic);
+      cs.code ~= proc6502.bytecode(mnemonic);
 
       // op1 is either absent or not a label
       if (op1 == "" || op1[0] != '.')
@@ -182,33 +200,33 @@ void main()
         }
         for (int i=0; i<op1.length; i += 2)
         {
-          machine_code ~= hexToByte(op1[i..i+2]);
+          cs.code ~= hexToByte(op1[i..i+2]);
         }
       }
 
       // op1 is a label
       else
       {
-        ushort codeIndex = cast(ushort)(machine_code.length);
+        ushort codeIndex = cast(ushort)(cs.code.length);
         string label = op1[1..$];
         final switch(proc6502.addrtype(mnemonic))
         {
           case ADDR_TYPE.ABS:
-            absAddrRef[codeIndex] = label;
-            machine_code ~= 0;
-            machine_code ~= 0;
+            cs.absAddrRef[codeIndex] = label;
+            cs.code ~= 0;
+            cs.code ~= 0;
             break;
           case ADDR_TYPE.IND:
             if (mnemonic == "jmpn")
             {
-              absAddrRef[codeIndex] = label;
-              machine_code ~= 0;
-              machine_code ~= 0;
+              cs.absAddrRef[codeIndex] = label;
+              cs.code ~= 0;
+              cs.code ~= 0;
             }
             else
             {
-              zpAddrRef[codeIndex] = label;
-              machine_code ~= 0;
+              cs.zpAddrRef[codeIndex] = label;
+              cs.code ~= 0;
             }
             break;
           case ADDR_TYPE.NONE:
@@ -216,12 +234,12 @@ void main()
             exit(1);
             break;
           case ADDR_TYPE.REL:
-            relAddrRef[codeIndex] = label;
-            machine_code ~= 0;
+            cs.relAddrRef[codeIndex] = label;
+            cs.code ~= 0;
             break;
           case ADDR_TYPE.ZP:
-            zpAddrRef[codeIndex] = label;
-            machine_code ~= 0;
+            cs.zpAddrRef[codeIndex] = label;
+            cs.code ~= 0;
             break;
         }
       }
@@ -229,67 +247,73 @@ void main()
   }
 
   // Insert address values
-
-  // Absolute addresses
-  foreach (codeIndex, label; absAddrRef)
+  foreach (code_seg cb; code_blocks)
   {
-    if (label !in labelToAddr)
+    // Absolute addresses
+    foreach (codeIndex, label; cb.absAddrRef)
     {
-      writefln("Undefined absolute address label: %s", label);
-      exit(1);
+      if (label !in labelToAddr)
+      {
+        writefln("Undefined absolute address label: %s", label);
+        exit(1);
+      }
+      ushort addr = labelToAddr[label];
+      // Little endian architecture
+      cb.code[codeIndex] = cast(ubyte)(addr & 0x00FF);
+      cb.code[codeIndex+1] = cast(ubyte)(addr >> 8);
     }
-    ushort addr = labelToAddr[label];
-    // Little endian architecture
-    machine_code[codeIndex] = cast(ubyte)(addr & 0x00FF);
-    machine_code[codeIndex+1] = cast(ubyte)(addr >> 8);
-  }
 
-  // Relative branch offsets
-  foreach (codeIndex, label; relAddrRef)
-  {
-    if (label !in labelToAddr)
+    // Relative branch offsets
+    foreach (codeIndex, label; cb.relAddrRef)
     {
-      writefln("Undefined branching label: %s", label);
-      exit(1);
+      if (label !in labelToAddr)
+      {
+        writefln("Undefined branching label: %s", label);
+        exit(1);
+      }
+      ushort addr = labelToAddr[label];
+      int offset = addr - 1 - (cb.org + codeIndex);
+      assert((offset >= -128) && (offset <= 127));
+      cb.code[codeIndex] = cast(ubyte)offset;
     }
-    ushort addr = labelToAddr[label];
-    int offset = addr - 1 - (org + codeIndex);
-    assert((offset >= -128) && (offset <= 127));
-    machine_code[codeIndex] = cast(ubyte)offset;
-  }
 
-  // Zero page addresses
-  foreach (codeIndex, label; zpAddrRef)
-  {
-    if (label !in labelToAddr)
+    // Zero page addresses
+    foreach (codeIndex, label; cb.zpAddrRef)
     {
-      writefln("Undefined zero page address label: %s", label);
-      exit(1);
+      if (label !in labelToAddr)
+      {
+        writefln("Undefined zero page address label: %s", label);
+        exit(1);
+      }
+      ushort addr = labelToAddr[label];
+      assert(addr < 256);
+      cb.code[codeIndex] = cast(ubyte)addr;
     }
-    ushort addr = labelToAddr[label];
-    assert(addr < 256);
-    machine_code[codeIndex] = cast(ubyte)addr;
   }
-
-  outputAppleIISystemMonitorInput(machine_code, org);
+  sort!("a.org < b.org")(code_blocks);
+  outputAppleIISystemMonitorInput(code_blocks);
 }
 
-void outputAppleIISystemMonitorInput(const ubyte[] code, ushort org)
+void outputAppleIISystemMonitorInput(in code_seg[] code_blocks)
 {
-  int byteNum = 0;
-  ushort loadAddr = org;
-  string loadAddrHex;
-  foreach (ubyte b; code)
+  foreach (const code_seg cs; code_blocks)
   {
-    if (byteNum % 83 == 0)
+    int byteNum = 0;
+    string loadAddrHex;
+    ushort loadAddr = cs.org;
+    foreach (ubyte b; cs.code)
     {
-      loadAddrHex = byteToHex(cast(ubyte)(loadAddr >> 8)) ~ byteToHex(cast(ubyte)(loadAddr & 0x00FF));
-      while ((loadAddrHex.length > 1) && (loadAddrHex[0] == '0')) loadAddrHex = loadAddrHex[1..$];
-      if (byteNum > 0) writeln();
-      writef("%s:", loadAddrHex);
-      loadAddr += 83;
+      if (byteNum % 83 == 0)
+      {
+        loadAddrHex = byteToHex(cast(ubyte)(loadAddr >> 8)) ~ byteToHex(cast(ubyte)(loadAddr & 0x00FF));
+        while ((loadAddrHex.length > 1) && (loadAddrHex[0] == '0')) loadAddrHex = loadAddrHex[1..$];
+        if (byteNum > 0) writeln();
+        writef("%s:", loadAddrHex);
+        loadAddr += 83;
+      }
+      writef("%s ", byteToHex(b));
+      byteNum++;
     }
-    writef("%s ", byteToHex(b));
-    byteNum++;
+    writeln();
   }
 }
